0f25c3d8797a55a7cdfb961b91617e915efddc8a
davinhster <vincentnishikino@gmail.com>
2019-12-19 12:34:09 -0800

    Changed port

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get('/courses', (req, res) => {
 });
 
 
-const server = app.listen(7000, () => {
+// const server = app.listen(7000, () => {
+//   console.log(`Express running → PORT ${server.address().port}`);
+// 
+const server = app.listen(process.env.PORT, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
